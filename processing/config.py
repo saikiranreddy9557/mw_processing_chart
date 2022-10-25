70867a216fe705bf0724dc2b8dfad2d0fc14e367
saikiranreddy9557 <62921078+saikiranreddy9557@users.noreply.github.com>
2022-10-25 20:30:03 +0530

    Update config.py

--- processing/config.py
+++ processing/config.py
@@ -5,18 +5,17 @@ first_iteration = False
 
 # Folder 177294206571 is named "test-excel-1"
 # Folder 177293289704 is named "test-pdf-1"
-folder_ids_list = ["177293289704","177294206571"]#test
-# ,'177293289704'
-# "177294206571",
+folder_ids_list = ["177294206571","177293289704"]#test
+
 
 region="ap-south-1"
 
 user_id='21158211249'
 
-table_name= "sample_table_dynamo"
-# 21158211249
+table_name= "sample_test_mw"
 
-# 17996867275
+bucket_name = "sample--bucket--doc"
+
 
 access_key ="AKIAW34VIHH3TRJ5IHSD"
 secret_key = "wRzvCqa8nIcUydaQQ2FL9EpFGWuzNrpAx6lNti91"
